db869a586f76ee87f17be99b014e46cf8bbeffa1
Anurag Adhikari <anuragadhikari5276@gmail.com>
2025-07-17 18:11:56 +0545

    Initial commit & file setup

---
+++ main.py
@@ -0,0 +1,9 @@
+from todo import TodoApp
+
+def main():
+    # main function to run cli todo app
+    app = TodoApp()
+    app.run()
+
+if __name__ == "__main__":
+    main()
---
+++ todo.py
@@ -0,0 +1,4 @@
+class TodoApp:
+    # run the cli app
+    def run(self) -> None:
+        print(f"CLI app is up and running")
